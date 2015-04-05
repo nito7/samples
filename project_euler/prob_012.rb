c220b483356f8985066f4ec8f54e4ca16ddc3394
nito7 <naoki.itoh.7kusa@gmail.com>
2015-04-06 01:57:14 +0900

    update

--- project_euler/prob_012.rb
+++ project_euler/prob_012.rb
@@ -5,14 +5,19 @@ m = 500
 i = 1
 
 while true
+  a = 0
   d = []
-  for j in 1..i
-    if i % j == 0 then
+  i.downto(1) do |x|
+    a += x
+  end
+  for j in 1..a
+    if a % j == 0 then
       d.push j
     end
   end
   if d.length > m then
     break
   end
+  i += 1
 end
 puts i
